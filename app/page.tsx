e552ebd5f82dbe9058fb8b47e3403cef54333ef1
Raul Val <raulval@outlook.com>
2023-01-09 15:02:41 -0300

    ✨ implement switches to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
+import { useState } from "react";
 import { ThemeProvider } from "styled-components";
+import Switches from "../components/Switches";
 import { GlobalStyle } from "../styles/global";
 import { Container, Title } from "../styles/home";
 import Theme from "../styles/theme";
 
 export default function Home() {
+  const [selectedSwitch, setSelectedSwitch] = useState("pomodoro");
+
   return (
     <>
       <title>Pomodoro App</title>
@@ -14,6 +18,7 @@ export default function Home() {
 
         <Container>
           <Title>pomodoro</Title>
+          <Switches onClick={(props) => setSelectedSwitch(props)} />
         </Container>
       </ThemeProvider>
     </>

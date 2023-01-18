6b16d7e89d0001bbea20e32957154d054300ba39
Raul Val <raulval@outlook.com>
2023-01-18 14:53:50 -0300

    🐛 fix localStorage is not defined

--- shared/utils/getLocalStorage.ts
+++ shared/utils/getLocalStorage.ts
@@ -1,5 +1,6 @@
 export const getLocalStorage = (key: string) => {
-  const value = localStorage.getItem(key);
+  const value =
+    typeof window !== "undefined" ? localStorage.getItem(key) : null;
   if (value) {
     return JSON.parse(value);
   }
